2976c843cb3be49ffa4ef145b16c08447ff4dc87
wi1dcard <wi1dcard@qq.com>
2018-10-05 16:02:49 +0800

    Add a document link

--- src/BaiduClient.php
+++ src/BaiduClient.php
@@ -106,6 +106,7 @@ class BaiduClient
      * @return string
      *
      * @see self::session()
+     * @see http://smartprogram.baidu.com/docs/develop/api/open_log/#%E7%94%A8%E6%88%B7%E6%95%B0%E6%8D%AE%E7%9A%84%E7%AD%BE%E5%90%8D%E9%AA%8C%E8%AF%81%E5%92%8C%E5%8A%A0%E8%A7%A3%E5%AF%86/
      */
     public function decrypt($cipherText, $iv, $sessionKey)
     {
